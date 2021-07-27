7f98d45941273cb3163d5568ea33ae5fb8d8d31e
Gagah Ghaniswara K <gagah.ghaniswara51@hotmail.com>
2021-07-28 01:18:07 +0700

    Feat : Colors ✨✨✨,Fix : Wrong Name

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -14,9 +14,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "0da20e7c28fed6e5f68050eca09ccfef",
-"/": "0da20e7c28fed6e5f68050eca09ccfef",
-"main.dart.js": "1250ed8d459820bec0d6632f747f7fb8",
+"index.html": "696653f81b6decdf1f42d3e52cc6689c",
+"/": "696653f81b6decdf1f42d3e52cc6689c",
+"main.dart.js": "386151000280d1e5b25d9842b2b156c8",
 "manifest.json": "99e4e1df91533a0b760bbe5262cc609a",
 "version.json": "40831c16fd8877ded69a864499ca6fb3"
 };
